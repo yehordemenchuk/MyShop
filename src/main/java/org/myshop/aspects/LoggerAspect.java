e896c849f9727b2f5af83865733353e2ee767411
Yehor Demenchuk <yehor.demenchuk@student.tuke.sk>
2025-02-08 23:08:47 +0100

    add AOP

---
+++ src/main/java/org/myshop/aspects/LoggerAspect.java
@@ -0,0 +1,51 @@
+package org.myshop.aspects;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+@Aspect
+@Component
+public class LoggerAspect {
+    private final Logger logger = Logger.getLogger(LoggerAspect.class.getName());
+
+    @Around("execution(* org.myshop..*.*(..))")
+    public Object log(ProceedingJoinPoint jointPoint) throws Throwable {
+        logger.info(jointPoint.getSignature().toShortString() + " starts execution");
+
+        Instant start = Instant.now();
+
+        Object proceedingRes = jointPoint.proceed();
+
+        Instant end = Instant.now();
+
+        long duration = Duration.between(start, end).toMillis();
+
+        logger.info("Execution time: " + duration + " ms");
+
+        logger.info(jointPoint.getSignature().toShortString() + " ends execution");
+
+        return proceedingRes;
+    }
+
+    @AfterThrowing(value = "execution(* org.myshop..*.*(..))", throwing = "ex")
+    public void log(JoinPoint joinPoint, Exception ex) {
+        logger.log(Level.SEVERE, joinPoint.getSignature().toShortString() +
+                " the exception was throwing due " + ex.getMessage());
+    }
+
+    @AfterReturning(value = "execution(* org.myshop..*.*(..))", returning = "retVal")
+    public void log(JoinPoint joinPoint, Object retVal) {
+        logger.log(Level.INFO, joinPoint.getSignature().toShortString() + " returns "
+                + (retVal == null ? "null" : retVal.toString()));
+    }
+}
