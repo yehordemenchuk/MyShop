dc007da11467f1f6b41ea239cb64865166acc3e6
Yehor Demenchuk <yehor.demenchuk@student.tuke.sk>
2025-02-14 22:06:13 +0100

    add EnableJpaRepositories and EntityScan annotations

--- src/main/java/org/myshop/MyShopApplication.java
+++ src/main/java/org/myshop/MyShopApplication.java
@@ -5,8 +5,12 @@ package org.myshop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories("org.myshop.repository")
+@EntityScan("org.myshop.models")
 public class MyShopApplication {
 
     public static void main(String[] args) {
