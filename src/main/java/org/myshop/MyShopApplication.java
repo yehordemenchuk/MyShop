52db0dcc540c9f1bfb55deb4d56924f8bb4885be
Yehor Demenchuk <yehor.demenchuk@student.tuke.sk>
2025-02-22 17:30:27 +0100

    fix logger

--- src/main/java/org/myshop/MyShopApplication.java
+++ src/main/java/org/myshop/MyShopApplication.java
@@ -6,11 +6,13 @@ package org.myshop;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaRepositories("org.myshop.repository")
 @EntityScan("org.myshop.models")
+@EnableAspectJAutoProxy
 public class MyShopApplication {
 
     public static void main(String[] args) {
